4af97a3edb0a877316f4e92cf35e90abd8153b44
Norief26 <norief26@hotmail.com>
2021-01-01 16:33:48 -0800

    Firebase: config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,13 +3,7 @@
 //************************************************************************************
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAF2f7fLMIWjj3i0mA9VKR8HE2582O-nPU",
-    authDomain: "project-5afa5.firebaseapp.com",
-    projectId: "project-5afa5",
-    storageBucket: "project-5afa5.appspot.com",
-    messagingSenderId: "656669935295",
-    appId: "1:656669935295:web:627aea20185cdb5ccaa80a",
-    measurementId: "G-Z9B597NLJ0"
+
 };
 
 export default firebaseConfig
